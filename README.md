069d0d7e1427257bdea2a1fb3d5205f2e916dfae
ttavenner <tommy.tavenner@gmail.com>
2014-11-08 08:03:08 -0500

    first Hapi commit with basic /corporate route

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-business_openva_api
+An API for [VABusinesses.org] (https://vabusinesses.org/)
 ===================
 
-a [Sails](http://sailsjs.org) application providing the API for business.openva.com.
+Built on [Hapi.js] (http://hapijs.com/) using [Elasticsearch] (http://www.elasticsearch.org).
+
+To reproduce the data set:
+
+1. Download and install [Elasticsearch] (http://www.elasticsearch.org)
+2. Start your Elasticsearch server
+3. Clone [Crump] (https://github.com/openva/crump)
+4. Download the [current data file] (https://s3.amazonaws.com/virginia-business/current.zip) and [the current address file] (https://s3.amazonaws.com/virginia-business/current.zip)
+or alternatively use Crump to download it
+5. Run `./Crump -tem`. If you want Crump to download the data for you add the -d option (i.e. `-tedm`)
+6. Run [the indexing script] (https://github.com/openva/vabusinesses.org/blob/master/index.sh), changing the directory to the location where you ran Crump.
+
+To install the API:
+
+1. Install Node
+2. Clone this repo
+3. `cd` to the directory where you installed the repo and run `npm install`.
+4. Alter the config.json file for your setup
+5. Run `node api.js`
+
+
---
+++ api.js
@@ -0,0 +1,119 @@
+var config = require('./config.json');
+
+var Hapi = require('hapi');
+var server = new Hapi.Server(config.port);
+
+server.pack.register({
+	plugin: require('elasticsearch-hapi-plugin'),
+	options: {
+		host: "http://" + config.elasticSearch.host + ":" + config.elasticSearch.port
+	}
+},
+function (err) {
+	if (err) {
+		console.error('Failed to load plugin: ', err);
+	}
+});
+
+var es = server.plugins['elasticsearch-hapi-plugin'].es;
+
+server.route({
+	method: 'GET',
+	path: '/',
+	handler: function (request, reply) {
+		reply('Business!');
+	}
+});
+
+server.route({
+	method: 'GET',
+	path: '/corporate/{name}',
+	handler: function (request, reply) {
+
+		es.search({
+			index: 'business',
+			type: "2",
+			body: {
+				query: {
+					match: {
+						name: request.params.name
+					}
+				}
+			}
+		}).then (function (resp) {
+			var response = [];
+			for (i = 0; i < resp.hits.hits.length; i++) {
+				response.push({name: resp.hits.hits[i]._source.name,
+					address: resp.hits.hits[i]._source.street_1,
+					city: resp.hits.hits[i]._source.city,
+					zip: resp.hits.hits[i]._source.zip,
+					industry: resp.hits.hits[i]._source.industry,
+					incorporation_date: resp.hits.hits[i]._source.incorporation_date
+				});
+			}
+			reply(response);
+		}, function (err) {
+			console.trace(err.message);
+		});
+	}
+});
+
+server.route({
+	method: 'GET',
+	path: '/lp/',
+	handler: function (request, reply) {
+		reply('LP!');
+	}
+});
+
+server.route({
+	method: 'GET',
+	path: '/amendments/',
+	handler: function (request, reply) {
+		reply('Amendments/!');
+	}
+});
+
+server.route({
+	method: 'GET',
+	path: '/officers/',
+	handler: function (request, reply) {
+		reply('Officers!');
+	}
+});
+
+server.route({
+	method: 'GET',
+	path: '/name/',
+	handler: function (request, reply) {
+		reply('Name!');
+	}
+});
+
+server.route({
+	method: 'GET',
+	path: '/merger/',
+	handler: function (request, reply) {
+		reply('Merger!');
+	}
+});
+
+server.route({
+	method: 'GET',
+	path: '/registered_names/',
+	handler: function (request, reply) {
+		reply('Registered Names!');
+	}
+});
+
+server.route({
+	method: 'GET',
+	path: '/llc/',
+	handler: function (request, reply) {
+		reply('LLC!');
+	}
+});
+
+server.start(function () {
+	console.log('Server running at: ', server.info.uri);
+});
